6f142aac03de24e1b848f16cca1732041a555ee2
Brandon Cisneros <brcisneros1991@gmail.com>
2024-12-02 16:49:14 -0500

    added: datasets & main file for Day 01_01

--- Day_01/main_01.go
+++ Day_01/main_01.go
@@ -1,3 +1,13 @@
+/*
+1. Sort each list from smallest to largest
+2. get the absolute-value difference between the respective line items
+3. Sum the differences
+4.
+
+
+
+*/
+
 package main
 
 func main() {
