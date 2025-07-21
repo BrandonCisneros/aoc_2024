d8855d7ed45276046f74a710011a7f1edc296d70
Brandon Cisneros <brcisneros1991@gmail.com>
2025-07-21 16:55:12 -0400

    started solution for Day 02

--- Day_02/main.go
+++ Day_02/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+)
 
+func check(e error) {
+	if e != nil {
+		println("There was an error!")
+		panic(e)
+	}
+}
+
+func main() {
+	//count := 0
+
+	data, err := os.Open("_sample.txt")
+	check(err)
+	scanner := bufio.NewScanner(data)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		println(line)
+	}
 }
