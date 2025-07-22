91098aca5e65fbf2bf31dc3fa3f5c07f62e5d3dd
Brandon Cisneros <brcisneros1991@gmail.com>
2025-07-22 14:08:40 -0400

    made 2 functions - asc  & desc

--- Day_02/main.go
+++ Day_02/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
@@ -13,15 +14,64 @@ func check(e error) {
 }
 
 func main() {
-	//count := 0
 
 	data, err := os.Open("_sample.txt")
 	check(err)
 	scanner := bufio.NewScanner(data)
 
+	count := 0
+
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		println(line)
+		fmt.Println(line)
+
+		diff := int(line[0]) - int(line[len(line)-1])
+
+		fmt.Println(diff)
+
+		switch diff < 0 {
+		case true:
+			verdict := asc(line)
+			fmt.Println(verdict)
+			if verdict == true {
+				count++
+			}
+		case false:
+			verdict := desc(line)
+			fmt.Println(verdict)
+			if verdict == true {
+				count++
+			}
+		}
+
 	}
+
+	fmt.Println("Count: ", count)
+}
+
+func asc(line string) bool {
+	fmt.Println("Entered ASC function...")
+	safe := false
+
+	for i := 0; i < int(len(line)-1); i++ {
+		fmt.Println("ASC i: ", i)
+
+	}
+
+	fmt.Println("asc safe: ", safe)
+	return safe
+
+}
+
+func desc(line string) bool {
+	fmt.Println("Entered DESC function...")
+	safe := false
+
+	for i := 0; i < int(len(line)-1); i++ {
+		fmt.Println("DESC i: ", i)
+
+	}
+
+	return safe
 }
