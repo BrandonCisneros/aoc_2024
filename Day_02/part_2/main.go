3a497504a8ce38e655241462f2ca9e456f619788
Brandon Cisneros <brcisneros1991@gmail.com>
2025-07-28 17:14:15 -0400

    completed Day 2 Part 2

--- Day_02/part_2/main.go
+++ Day_02/part_2/main.go
@@ -20,119 +20,122 @@ func main() {
 	data, err := os.Open("_full.txt")
 	check(err)
 	scanner := bufio.NewScanner(data)
+	reports := [][]int{}
 
 	count := 0
-	lineCount := 1
 
 	for scanner.Scan() {
 		line := scanner.Text()
+		repList := []int{}
 
 		valList := strings.Split(line, " ")
-		numList := []int{}
 
-		for _, i := range valList {
-			i, err := strconv.Atoi(i)
+		for i := range valList {
+			val, err := strconv.Atoi(valList[i])
+			repList = append(repList, val)
 			check(err)
-			numList = append(numList, i)
 		}
 
-		diff := numList[0] - numList[len(numList)-1]
+		reports = append(reports, repList)
+	}
 
-		println("Line: ", lineCount)
-		lineCount++
+	fmt.Println("Reports: ", reports)
 
-		fmt.Println("Num1: ", numList[0], " | Num2: ", numList[len(numList)-1], " | Diff: ", diff)
+	for _, data := range reports {
+		fmt.Println("Reports Data: ", data)
 
-		switch {
-		case diff < 0:
-			verdict := asc(numList)
-			if verdict {
-				count++
-			}
-		case diff > 0:
-			verdict := desc(numList)
-			if verdict {
-				count++
-			}
+		//--- This works because the if statement will
+		//--- only evaluate true cases; Anything that
+		//--- doesn't meet base case will not increment count
+		if validate(data) || dampener(data) {
+			count++
 		}
-
 	}
 
 	fmt.Println("Count: ", count)
 }
 
-func asc(line []int) bool {
-	println("Entered ASC function...")
+func validate(data []int) bool {
+	incr := allIncreasing(data)
+	decr := allDecreasing(data)
+	min := minDiff(data, 1)
+	max := maxDiff(data, 3)
 
-	safe := true
-	valList := line
-	badLevels := 0
-
-	fmt.Println("Line: ", line, " | valList: ", valList)
-
-	for i := 1; i < int(len(valList)-1); i++ {
-		fmt.Println("ASC i: ", valList[i])
-
-		diff := valList[i] - valList[i-1]
-		println("Diff: ", diff)
-
-		if valList[i] < valList[i+1] && diff >= 1 && diff <= 3 {
-			println(valList[i], " | ", valList[i+1])
-			continue
-		} else {
-			fmt.Println("Processing Bad Lists...")
-			for i := 1; i < len(valList)-1; i++ {
-				line := valList
-				copyLine := make([]int, len(line))
-				copy(copyLine, line)
-				lineWrapper := append(copyLine[:i], copyLine[i+1:]...)
-				fmt.Println("Line Wrapper: ", lineWrapper)
-
-				diff := lineWrapper[i] - lineWrapper[i-1]
-				println("Diff: ", diff)
-
-				if lineWrapper[i] < lineWrapper[i+1] && diff >= 1 && diff <= 3 {
-					println(lineWrapper[i], " | ", lineWrapper[i+1])
-					continue
-				} else {
-					badLevels++
-				}
-
-			}
-		}
-	}
-
-	fmt.Println("Bad Levels: ", badLevels)
-	if badLevels > 1 {
-		safe = false
-	}
-
-	fmt.Println("asc safe: ", safe)
-	return safe
-
+	return (incr || decr) && min && max
 }
 
-func desc(line []int) bool {
-	fmt.Println("Entered DESC function...")
-	safe := true
-
-	valList := line
-
-	fmt.Println("Line: ", line, " | valList: ", valList)
-
-	for i := 0; i < int(len(valList)-1); i++ {
-		//fmt.Println("ASC i: ", valList[i])
-
-		diff := valList[i] - valList[i+1]
-		//println("Diff: ", diff)
-
-		if valList[i] > valList[i+1] && diff <= 3 {
-			continue
-		} else {
-			safe = false
+func dampener(data []int) bool {
+	//--- This approach is creating a different slice each loop
+	//--- by removing the index value from the orginal slice.
+	//--- This a brute force method.
+	for i := 0; i < len(data); i++ {
+		data1 := append([]int{}, data[:i]...)
+		data1 = append(data1, data[i+1:]...)
+		if validate(data1) {
+			return true
 		}
-
 	}
-	println("desc safe:", safe)
-	return safe
+
+	return false
+}
+
+func allIncreasing(data []int) bool {
+	//--- The approach of the assumption is true unless it's proven no to be.
+	//--- The loop approach keeps everything within the index bounds
+	for i := 1; i < len(data); i++ {
+		if data[i] < data[i-1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func allDecreasing(data []int) bool {
+	//--- The approach of the assumption is true unless it's proven no to be.
+	//--- The loop approach keeps everything within the index bounds
+	for i := 1; i < len(data); i++ {
+		if data[i] > data[i-1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func minDiff(data []int, min int) bool {
+	//--- The assumtion is true unless proven false.
+	//--- we send the int to an absolute value function
+	//--- so that we can make negatives absolute without
+	//--- converting the int to a float using the bult-in absolute value function.
+
+	for i := 1; i < len(data); i++ {
+		if absVal(data[i]-data[i-1]) < min {
+			return false
+		}
+	}
+	return true
+}
+
+func maxDiff(data []int, max int) bool {
+	//--- The assumtion is true unless proven false.
+	//--- we send the int to an absolute value function
+	//--- so that we can make negatives absolute without
+	//--- converting the int to a float using the bult-in absolute value function.
+
+	for i := 1; i < len(data); i++ {
+		if absVal(data[i]-data[i-1]) > max {
+			return false
+		}
+	}
+
+	return true
+}
+
+func absVal(num int) int {
+	if num < 0 {
+		return -1 * num
+	}
+
+	return num
 }
